139b93bc7f9d98ecc4f5fdb8de4664b95203961b
Sudhish Cherukuri <scheruk1@charlotte.edu>
2024-11-02 16:37:58 -0400

    implementing CSV file creation for stock data

--- main.go
+++ main.go
@@ -10,6 +10,28 @@ type Stock struct {
     Change string
 }
 
+var stocks []Stock
+
 func main() {
-	fmt.Println("Compile Crew")
+	// fmt.Println("Compile Crew")
+
+}
+
+func loadStockData() {
+
+	//setup for csv file writing
+	file, err := os.Create("stocks_data.csv")
+	if err != nil {
+		fmt.Println("Error creating CSV file:", err)
+		return
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	headers := []string{"Symbol", "Price", "Change"}
+	writer.Write(headers)
+
+
 }
